1a60327449d6a6da9c6e202c3f32acaa27fae2e7
Danilo Carvalho <dancrv.fx3@gmail.com>
2024-10-31 15:08:14 -0300

    hello world

--- ProdutosAPI/src/main/java/https/github/com/DanCrvX/ProdutosAPI/ProdutosApiApplication.java
+++ ProdutosAPI/src/main/java/https/github/com/DanCrvX/ProdutosAPI/ProdutosApiApplication.java
@@ -2,9 +2,17 @@ package https.github.com.DanCrvX.ProdutosAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProdutosApiApplication {
+
+		@GetMapping("/hello world")
+		public String helloworld() {
+		return "Hello World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosApiApplication.class, args);
