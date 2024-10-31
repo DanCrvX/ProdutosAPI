cf40b62233151f929836ef38fedec9c36302e443
Danilo Carvalho <dancrv.fx3@gmail.com>
2024-10-31 14:47:07 -0300

    Initial commit

---
+++ ProdutosAPI/src/main/java/https/github/com/DanCrvX/ProdutosAPI/ProdutosApiApplication.java
@@ -0,0 +1,15 @@
+package https.github.com.DanCrvX.ProdutosAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosApiApplication.class, args);
+	}
+
+
+
+}
---
+++ ProdutosAPI/src/test/java/https/github/com/DanCrvX/ProdutosAPI/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.DanCrvX.ProdutosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
